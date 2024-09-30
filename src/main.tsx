a723a85517124b7bbfe4b3f9b6cedf0761cdf878
HoodaHashem <mahmoudhashem335@gmail.com>
2024-09-30 21:28:27 +0300

    some refactor

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/index.css";
-import { ModalProvider } from "./providers/ModalProvider.tsx";
-import { LoadingProvider } from "./providers/LoadingProvider.tsx";
+import { ModalProvider } from "./providers/ModalProvider";
+import { LoadingProvider } from "./providers/LoadingProvider";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <LoadingProvider>
