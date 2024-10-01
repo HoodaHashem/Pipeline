76e151b28d4879a6b3ba7549e4e8589ca0ae4ba0
HoodaHashem <mahmoudhashem335@gmail.com>
2024-10-01 18:57:51 +0300

    basic structure for app page

--- src/pages/app/Layout.tsx
+++ src/pages/app/Layout.tsx
@@ -1,15 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Modal from "../../components/Ui/Modal";
 import InternalServerError from "../../components/Ui/InternalServerError";
-import Sidebar from "../../components/App/Sidebar/Sidebar";
 import useInternalServerError from "../../hooks/useInternalServerError";
+import Sidebar from "../../components/App/Sidebar";
 
 const AppLayout = () => {
   const { isInternalServerError, setIsInternalServerError } =
     useInternalServerError();
   return (
-    <div className="bg-bg flex flex-row ">
+    <div className=" bg-bg flex flex-row ">
       <Sidebar />
+
       <Modal
         open={isInternalServerError}
         onClose={() => setIsInternalServerError(false)}
--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,4 +1,4 @@
 const app = () => {
-  return <h1>App page</h1>;
+  return <h1> app </h1>;
 };
 export default app;
