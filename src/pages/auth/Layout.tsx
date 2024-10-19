c4c38286ffdfb97f36131c75b6109ea37801b982
HoodaHashem <mahmoudhashem335@gmail.com>
2024-10-19 16:55:15 +0300

    auth route edit

--- src/pages/auth/Layout.tsx
+++ src/pages/auth/Layout.tsx
@@ -1,10 +1,37 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Navbar from "../../components/Header/Navbar";
 import InternalServerError from "../../components/Ui/InternalServerError";
 import useInternalServerError from "../../hooks/useInternalServerError";
+import { useEffect, useState } from "react";
+import { isLoggedIn } from "../../lib/apiCenter";
+import PrimaryLoader from "../../components/Ui/PrimaryLoader";
 const AuthLayout = () => {
   const { isInternalServerError, setIsInternalServerError } =
     useInternalServerError();
+  const [isAuth, setIsAuth] = useState<boolean | null>(null);
+
+  const authenticate = async () => {
+    const result = await isLoggedIn();
+    console.log(result);
+    if (result.status === "success") {
+      setIsAuth(true);
+    }
+    if (result.status === "fail") {
+      setIsAuth(false);
+    }
+    if (result.status === "error") {
+      setIsAuth(false);
+    }
+  };
+
+  useEffect(() => {
+    authenticate();
+  }, []);
+
+  if (isAuth === null) return <PrimaryLoader />;
+
+  if (isAuth === true) return <Navigate to={"/app"} replace />;
+
   return (
     <div className="h-screen bg-bg ">
       <Navbar />
