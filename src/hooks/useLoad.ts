1f3e72b31197be09b3528a6d4ba6a2b6b445fd15
HoodaHashem <mahmoudhashem335@gmail.com>
2024-11-19 16:48:26 +0200

    update

--- src/hooks/useLoad.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import LoadingContext from "../contexts/LoadingContext";
-
-const useLoad = () => {
-  const context = useContext(LoadingContext);
-  return context;
-};
-
-export default useLoad;
